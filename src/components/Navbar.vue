06a6b4f549218193ff6fa4311f5258f91c22ed25
Marko <marko123obradovic@gmail.com>
2022-11-30 18:39:52 +0100

    added component toggler

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="w-full flex flex-col fixed top-0">
-    <div class="hidden sm:flex py-4 px-12 md:px-24  bg-gray-200 border-b-[0.1rem] border-gray-400 items-center justify-between">
+  <div class="w-full flex flex-col fixed top-0 z-10">
+    <div class="hidden sm:flex py-4 px-12 md:px-24  bg-gray-200 border-b-[0.1rem] border-gray-400 
+                items-center justify-between">
       <div>
         <span class="text-[1.1rem] md:text-xl font-semibold">Introducing our 2022 Winter Release</span>
       </div>
@@ -8,26 +9,34 @@
         <a href="">Explore what's new</a>
       </div>
     </div>
-    <div class="px-2 sm:px-24 border-b-[0.1rem] bg-white border-gray-400 flex items-start justify-between">
-      <div class="flex items-center">
+    <div class="px-2 py-3 sm:py-0 sm:pb-0 lg:px-24 shadow-lg sm:shadow-md bg-white border-gray-400 flex items-center sm:justify-between justify-center">
+
+      <div class="items-center hidden sm:flex">
         <div class="w-20 h-20">
           <img class="w-full h-full" src="../assets/home_logo.png" alt="">
         </div>
-        <span class="font-semibold text-lg hidden sm:block">ExampleName</span>
+        <span class="font-semibold text-lg hidden md:block">ExampleName</span>
       </div>
-      
-      <div>
 
-      </div>
-      <div>
+      <Search/>
 
+      <div class="items-center gap-2 hidden sm:flex">
+        <a href="">Language</a>
+        <div class="border-[0.1rem] border-gray-400 rounded-2xl p-2">
+          <a href="" class="font-semibold">Profile</a>
+        </div>
       </div>
+
     </div>
   </div>
 </template>
 
 <script>
+import Search from './Search-Components/Search_Toggler.vue' 
 export default{
+  components:{
+    Search
+  },
   computed:{
 
   },
