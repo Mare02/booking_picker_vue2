11ad71775954d09e5201dcd0bc48fbf34d71aa02
Marko <marko123obradovic@gmail.com>
2022-12-04 17:47:42 +0100

    done

--- src/components/Search-Components/Search_Controller.vue
+++ src/components/Search-Components/Search_Controller.vue
@@ -53,7 +53,7 @@
             </div>
 
             <div class="absolute right-5 bg-gray-200 p-1 rounded-full hover:bg-gray-300 transition-all hidden sm:block"
-              v-show="selected_picker == 'location_picker' && location_query !== '' || selected_flex_opt"
+              v-show="selected_picker == 'location_picker' && (location_query !== '' || selected_region)"
               @click="clearData()">
               <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M18.3 5.71c-.39-.39-1.02-.39-1.41 0L12 10.59 7.11 5.7c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41L10.59 12 5.7 16.89c-.39.39-.39 1.02 0 1.41.39.39 1.02.39 1.41 0L12 13.41l4.89 4.89c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4z"/></svg>
             </div>
